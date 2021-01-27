65e8d3bd8ca1a5d1332f7a2acf50aa648e38264b
github_rajSani9685 <rajkumar.siwan333@gmail.com>
2021-01-27 20:56:10 +0530

    added variable

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -10,6 +10,7 @@ public class Application {
 	public static void main(String[] args) {
 		//change in my status-hb-134
 		ConfigurableApplicationContext run=SpringApplication.run(Application.class,args);
+int a=10;
 		run.close();
 	}
 
